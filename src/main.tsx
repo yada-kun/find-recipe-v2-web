17905eaec67851f5d91f0faa099d5169f7aca6d8
Mark Christian Fajardo <cfajardo25@gmail.com>
2022-10-16 16:50:29 +0800

    Feat: Add CssBaseLine, Inter font, and change import location of SxProps

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import App from './App';
 import './index.css';
@@ -9,6 +10,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <CssBaseline />
       <App />
     </BrowserRouter>
   </React.StrictMode>
